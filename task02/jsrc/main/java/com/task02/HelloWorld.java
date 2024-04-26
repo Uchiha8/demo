cc0900a547297d8a163ac1699f83c0ce3c8e0cec
Uchiha8 <najimovm11@gmail.com>
2024-04-26 18:02:29 +0500

    done

---
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -0,0 +1,32 @@
+package com.task02;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.RetentionSetting;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@LambdaHandler(lambdaName = "hello_world",
+	roleName = "hello_world-role",
+	isPublishVersion = true,
+	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@LambdaUrlConfig
+public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
+
+	public Map<String, Object> handleRequest(Object request, Context context) {
+		System.out.println("Hello from lambda");
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("statusCode", 200);
+		resultMap.put("message", "Hello from Lambda");
+		resultMap.put("body", "{\n" +
+				" \"statusCode\": 200,\n" +
+				" \"message\": \"Hello from Lambda\"\n" +
+				" }");
+
+		return resultMap;
+	}
+}
+
