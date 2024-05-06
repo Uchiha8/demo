0c087b63651fee87f626b0ae455f02147ea66f8c
Uchiha8 <najimovm11@gmail.com>
2024-05-06 15:59:16 +0500

    done

--- task06/jsrc/main/java/com/task06/AuditProducer.java
+++ task06/jsrc/main/java/com/task06/AuditProducer.java
@@ -5,6 +5,8 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
+import com.syndicate.deployment.annotations.events.DynamoDbTriggerEventSource;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,6 +14,7 @@ import java.util.Map;
 	roleName = "audit_producer-role",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
+@DynamoDbTriggerEventSource(targetTable = "Configuration", batchSize = 10)
 public class AuditProducer implements RequestHandler<Object, Map<String, Object>> {
 
 	public Map<String, Object> handleRequest(Object request, Context context) {
