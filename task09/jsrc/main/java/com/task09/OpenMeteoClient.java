e11b9b11ba3ff24dd36b9e0ca397cade743e07d0
Uchiha8 <2110107@newuu.uz>
2024-05-14 14:51:01 +0500

    done

---
+++ task09/jsrc/main/java/com/task09/OpenMeteoClient.java
@@ -0,0 +1,27 @@
+package com.task09;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class OpenMeteoClient {
+
+    public HttpResponse<String> getCurrentWeather() {
+        HttpRequest request;
+        HttpResponse<String> response;
+        try {
+            String CURRENT_WEATHER_URI = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m";
+            request = HttpRequest.newBuilder()
+                    .uri(new URI(CURRENT_WEATHER_URI))
+                    .GET()
+                    .build();
+            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return response;
+    }
+}
---
+++ task09/jsrc/main/java/com/task09/Processor.java
@@ -0,0 +1,67 @@
+package com.task09;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.TracingMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@LambdaHandler(lambdaName = "processor",
+        roleName = "processor-role",
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED,
+        tracingMode = TracingMode.Active
+)
+@LambdaUrlConfig
+public class Processor implements RequestHandler<Object, Map<String, Object>> {
+
+    private static final Logger log = LoggerFactory.getLogger(Processor.class);
+    private final OpenMeteoClient weatherClient = new OpenMeteoClient();
+    private static final String TABLE_NAME = "cmtr-580435c6-Weather-test";
+    private final AmazonDynamoDB client = AmazonDynamoDBClientBuilder.defaultClient();
+    private final DynamoDB dynamoDB = new DynamoDB(client);
+
+    public Map<String, Object> handleRequest(Object request, Context context) {
+        //get weather
+        HttpResponse<String> currentWeather = weatherClient.getCurrentWeather();
+        int statusCode = currentWeather.statusCode();
+        String body = currentWeather.body();
+        //add to dynamoDB
+        addWeatherToDynamoDB(body);
+
+        log.info("Hello from Processor");
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        resultMap.put("statusCode", statusCode);
+        resultMap.put("body", body);
+        return resultMap;
+
+    }
+
+    private void addWeatherToDynamoDB(String body) {
+        //add to dynamoDB
+        log.info("Body: {}", body);
+        String id = UUID.randomUUID().toString();
+        Item item = new Item()
+                .withString("id", id)
+                .withJSON("forecast", body);
+        try {
+            Table weatherTable = dynamoDB.getTable(TABLE_NAME);
+            weatherTable.putItem(item);
+            log.info("Item added to DynamoDB");
+        } catch (Exception e) {
+            log.error("Error adding item to DynamoDB", e);
+        }
+    }
+}
